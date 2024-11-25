9cb0214140b676e7f87b676a179d6557666e4581
IqraAbdulQadir <iqraabdulqadir05@gmail.com>
2024-11-25 12:09:12 +0500

    Fixed unused error variable issue in route.ts

--- app/api/books/route.ts
+++ app/api/books/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
 
-
 let books = [
   {
     id: 1,
@@ -25,77 +24,76 @@ let books = [
 ];
 
 export async function GET () {
-  
     console.log('API HAS BEEN CONNECTED');
     try {
-        
-        return NextResponse.json(books, { status: 200});
+        return NextResponse.json(books, { status: 200 });
     }
-    catch (error) { 
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    catch (error) {
+        console.error(error)
+
         return NextResponse.json(
-            {message: "Error fetching books"},
+            { message: "Error fetching books" },
             { status: 500 }
         );
+    }
 };
-};
-
-
-  
 
 export async function POST (request : Request) {
     try {
         const newBook = await request.json();
-        books.push({...newBook, id: books.length + 1});
+        books.push({ ...newBook, id: books.length + 1 });
         return NextResponse.json(
-            {message: "Books added sucessfully"},
-            {status: 202},
-        )
-    }
-
-    catch(error) {
-        return NextResponse.json(
-            {message: "Error adding book"},
-            {status: 500},
+            { message: "Books added successfully" },
+            { status: 202 }
         );
-    };
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    catch (error) {
+        console.error(error)
+
+        return NextResponse.json(
+            { message: "Error adding book" },
+            { status: 500 }
+        );
+    }
 };
 
 export async function PUT (request: Request) {
     try {
         const updatedBook = await request.json();
-        books = books.map(book=> (book.id === updatedBook.id ? updatedBook : book ) );
+        books = books.map(book => (book.id === updatedBook.id ? updatedBook : book));
         return NextResponse.json(
-            {message: "Book sucessfully updated"},
-            {status:200},
+            { message: "Book successfully updated" },
+            { status: 200 }
         );
     }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    catch (error) {
+        console.error(error)
 
-    catch(error) {
         return NextResponse.json(
-            {message: "Error updating books"},
-            {status: 500}
-        )
+            { message: "Error updating books" },
+            { status: 500 }
+        );
     }
-
 }
 
 export async function DELETE (request: Request) {
     try {
-      const { id } = await request.json();
-      books = books.filter(book => book.id !== id);
-      return NextResponse.json(
-        { message: "Book deleted successfully" },
-        {status: 200},
-      )
-
+        const { id } = await request.json();
+        books = books.filter(book => book.id !== id);
+        return NextResponse.json(
+            { message: "Book deleted successfully" },
+            { status: 200 }
+        );
     }
-
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     catch (error) {
-        return NextResponse.json (
-            {message: "Error deleting book"},
-            {status: 200},
-        )
+        console.error(error)
+        return NextResponse.json(
+            { message: "Error deleting book" },
+            { status: 500 }
+        );
     }
-
 }
-
